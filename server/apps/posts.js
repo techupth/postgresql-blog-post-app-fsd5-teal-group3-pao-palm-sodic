b2be2c0655745a112b1daeb2da0de631c5bd1abc
Sodikcp <paramesh.rah@gmail.com>
2024-01-24 15:49:17 +0700

    feat: complete postgresql node

--- server/apps/posts.js
+++ server/apps/posts.js
@@ -1,22 +1,48 @@
 import { Router } from "express";
+import { pool } from "../utils/db.js";
 
 const postRouter = Router();
 
 postRouter.get("/", async (req, res) => {
-  const status = req.query.status;
-  const keywords = req.query.keywords;
-  const page = req.query.page;
+  const status = req.query.status || "";
+  const keywords = req.query.keywords || "";
+  const page = req.query.page || 1;
+  const PAGE_SIZE = 5;
+  const offset = (page - 1) * PAGE_SIZE;
+
+  let query = "";
+  let values = [];
+
+  if (status && keywords) {
+    query = `select * from posts where status=$1 and title ilike $2 limit $3 offset $4`;
+    values = [status, keywords, PAGE_SIZE, offset];
+  } else if (status) {
+    query = `select * from posts where status=$1 limit $2 offset $3`;
+    values = [status, PAGE_SIZE, offset];
+  } else if (keywords) {
+    query = `select * from posts where title ilike $1 limit $2 offset $3`;
+    values = [keywords, PAGE_SIZE, offset];
+  } else {
+    query = `select * from posts limit $1 offset $2`;
+    values = [PAGE_SIZE, offset];
+  }
+
+  const result = await pool.query(query, values);
 
   return res.json({
-    data: [],
+    data: result.rows,
   });
 });
 
 postRouter.get("/:id", async (req, res) => {
   const postId = req.params.id;
 
+  const result = await pool.query("select * from posts where post_id=$1", [
+    postId,
+  ]);
+
   return res.json({
-    data: {},
+    data: result.rows[0],
   });
 });
 
@@ -29,6 +55,24 @@ postRouter.post("/", async (req, res) => {
     published_at: hasPublished ? new Date() : null,
   };
 
+  await pool.query(
+    `insert into posts (user_id,title,
+    content,status,likes,category,created_at,updated_at,
+    published_at)
+    values($1,$2,$3,$4,$5,$6,$7,$8,$9)`,
+    [
+      newPost.user_id,
+      newPost.title,
+      newPost.content,
+      newPost.status,
+      newPost.likes,
+      newPost.category,
+      newPost.created_at,
+      newPost.updated_at,
+      newPost.published_at,
+    ]
+  );
+
   return res.json({
     message: "Post has been created.",
   });
@@ -36,13 +80,31 @@ postRouter.post("/", async (req, res) => {
 
 postRouter.put("/:id", async (req, res) => {
   const hasPublished = req.body.status === "published";
-
   const updatedPost = {
     ...req.body,
     updated_at: new Date(),
     published_at: hasPublished ? new Date() : null,
   };
   const postId = req.params.id;
+  try {
+    await pool.query(
+      `update posts set title=$1,content=$2,
+  status=$3,category=$4,updated_at=$5,published_at=$6 where post_id=$7`,
+      [
+        updatedPost.title,
+        updatedPost.content,
+        updatedPost.status,
+        updatedPost.category,
+        updatedPost.updated_at,
+        updatedPost.published_at,
+        postId,
+      ]
+    );
+  } catch {
+    return res.status(400).json({
+      message: "Plese check body",
+    });
+  }
 
   return res.json({
     message: `Post ${postId} has been updated.`,
@@ -51,6 +113,13 @@ postRouter.put("/:id", async (req, res) => {
 
 postRouter.delete("/:id", async (req, res) => {
   const postId = req.params.id;
+  try {
+    await pool.query("delete from posts where post_id=$1", [postId]);
+  } catch {
+    return res.status(400).json({
+      message: "Error can not delete",
+    });
+  }
 
   return res.json({
     message: `Post ${postId} has been deleted.`,
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,10 @@
 // Creating PostgreSQL Client here
+import * as pg from "pg";
+
+const { Pool } = pg.default;
+
+const pool = new Pool({
+  connectionString: "postgresql://postgres:1234@localhost:5432/node",
+});
+
+export { pool };
